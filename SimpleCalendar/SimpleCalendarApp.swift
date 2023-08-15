946f262fe363917af3d0507843a915026c180ec1
Ege Sucu <egesucu@icloud.com>
2023-08-15 19:49:39 +0300

    Initial Commit

---
+++ SimpleCalendar/SimpleCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalendarApp.swift
+//  SimpleCalendar
+//
+//  Created by Ege Sucu on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
